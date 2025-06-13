6ac58a5f5f111c1355c958560564a1f89382581e
lauryn <laurynmwase06@gmail.com>
2025-06-13 16:39:12 +0200

    implemented the buttons functionality of displaying on click

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-add=()=>{
+add=(a,b)=>{
     return a+b;
 }
-subtract=()=>{
+subtract=(a,b)=>{
     return a-b;
 }
-multiply=()=>{
+multiply=(a,b)=>{
     return a*b;
 }
-divide=()=>{
-    return a/b;
+divide=(a,b)=>{
+    if(b==0){
+        return "error";
+    }else{
+ return a/b;
+    }
 }
-let a,b,operation;
 operate=(operation,a,b)=>{
-add();
+if(operation==="+"){
+   return  add(a,b);
 }
+else if(operation==="-"){
+   return subtract(a,b);
+}
+else if(operation==="*"){
+    return multiply(a,b);
+}
+else if(operation==="/"){
+     return divide(a,b);
+
+}else{
+    return "error";
+}
+
+}
+console.log(operate("+",4,6));
+console.log(operate("-",4,6));
+console.log(operate("/",4,6));
+console.log(operate("*",4,6));
+
+document.addEventListener("DOMContentLoaded",function(){
+
+const numbers =document.querySelectorAll(".digit");
+const display=document.querySelector(".clear");
+let displayValue="";
+
+numbers.forEach(button => {
+    button.addEventListener("click",()=>{
+    const num =button.textContent.trim();
+
+    if(displayValue==="0"){
+        displayValue=num;
+    }
+    else{
+        displayValue += num;
+    }
+    display.textContent=displayValue;
+        });
+});
+});
+
