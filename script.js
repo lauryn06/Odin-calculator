e54804a9133ba77940ea7fe15cc2ba8754287078
lauryn <laurynmwase06@gmail.com>
2025-06-13 14:41:47 +0200

    added the operations on the js file

---
+++ script.js
@@ -0,0 +1,16 @@
+add=()=>{
+    return a+b;
+}
+subtract=()=>{
+    return a-b;
+}
+multiply=()=>{
+    return a*b;
+}
+divide=()=>{
+    return a/b;
+}
+let a,b,operation;
+operate=(operation,a,b)=>{
+add();
+}
