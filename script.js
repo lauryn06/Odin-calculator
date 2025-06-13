145352a8baac4f32e2deb3f53c748136fb18faf1
lauryn <laurynmwase06@gmail.com>
2025-06-13 18:32:13 +0200

    Adding final functionaliies of the operators

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-add=(a,b)=>{
-    return a+b;
-}
-subtract=(a,b)=>{
-    return a-b;
-}
-multiply=(a,b)=>{
-    return a*b;
-}
-divide=(a,b)=>{
-    if(b==0){
-        return "error";
-    }else{
- return a/b;
-    }
-}
-operate=(operation,a,b)=>{
-if(operation==="+"){
-   return  add(a,b);
-}
-else if(operation==="-"){
-   return subtract(a,b);
-}
-else if(operation==="*"){
-    return multiply(a,b);
-}
-else if(operation==="/"){
-     return divide(a,b);
 
-}else{
-    return "error";
-}
-
-}
-console.log(operate("+",4,6));
-console.log(operate("-",4,6));
-console.log(operate("/",4,6));
-console.log(operate("*",4,6));
 
 document.addEventListener("DOMContentLoaded",function(){
 
@@ -56,5 +19,76 @@ numbers.forEach(button => {
     display.textContent=displayValue;
         });
 });
-});
 
+
+const  numberButton=document.querySelectorAll(".digit");
+const clearButton=document.querySelector(".del");
+const equalButton=document.querySelector(".eq");
+const operatorButton=document.querySelectorAll(".op");
+
+let firstNumber="";
+let secondNumber="";
+let currentOperator="";
+let  isEnteringSecond=false;
+
+clearButton.addEventListener("click",()=>{
+    firstNumber="";
+    secondNumber="";
+        currentOperator="";
+        isEnteringSecond=false;
+    display.textContent=0;
+});
+numberButton.forEach(button=>{
+    button.addEventListener("click",()=>{
+        const v=button.textContent
+        if(!isEnteringSecond){
+        firstNumber +=v;
+        display.textContent=firstNumber;
+        }else{
+            secondNumber += v;
+            display.textContent =secondNumber;
+        }
+    })
+})
+operatorButton.forEach(button=>{
+    button.addEventListener("click",()=>{
+        currentOperator=button.textContent.trim();
+        isEnteringSecond=true;
+    })
+})
+    equalButton.addEventListener("click",()=>{
+        display.textContent=operate(currentOperator,Number(firstNumber),Number(secondNumber));
+    });
+ operate=(operator, a, b) =>{
+    if(operator==="+"){
+        return add(a, b);
+    }else if(operator==="-"){
+return subtract(a, b);
+    }else if(operator==="*"){
+        return multiply(a, b); 
+    }else if (operator==="/"){
+ return divide(a, b);
+    }else if(operator==="%"){
+return a % b;
+    }else{
+return "SyntaxError";
+    }
+}
+add=(a,b)=>{
+    return a+b;
+}
+subtract=(a,b)=>{
+    return a-b;
+}
+multiply=(a,b)=>{
+    return a*b;
+}
+divide=(a,b)=>{
+    if(b==0){
+        return "error";
+    }else{
+ return a/b;
+    }
+}
+
+});
